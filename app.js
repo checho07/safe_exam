da33c7e867bfc2d99f12a411cdc738a1d983afbd
Javier Garcia <javier.garcia9611@gmail.com>
2020-09-04 15:55:36 -0500

    En el archivo app.js se actualiza la creación de usuario al ingresar con cuenta Google, se crea en la colección estudiantes un documento de ID igual al correo del usuario, y se agregar los campos de correo, foto y nombre. Si en la autenticación se reconoce que existe un documento con ID igual se actualizan los campos de correo, foto y nombre por medio de la función ActualizarDatosUusario la cual también fue modificada para enlazar directamente al documento en Firebase del usuario actual. Se agrega la función VerificarEstado que determina si un usuario ya tiene sesión iniciada o no, redirigiendo a inicio.html o advirtiendo que no hay sesión activa. Esta función también es implementada en la función AccederConCuentaGoogle una vez el usuario es creado y se actualiza su información para luego redirigir a inicio.html, esto con el fin de permitir que una vez sus datos se actualicen se pueda avanzar a la siguiente página.

--- app.js
+++ app.js
@@ -1,91 +1,44 @@
 //Verficar si hay sesión iniciada
-function redireccionar_inicio() {
-  location.href = "inicio.html";
-}
-
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    //document.getElementById('BotonEntrar').style.visibility = 'hidden';
-    setTimeout(redireccionar_inicio(), 1000);
-    console.log("usuario activo")
-
-  } else {
-    console.log("usuario inactivo")
-    //document.getElementById('BotonEntrar').style.visibility = 'visible';
-  }
-});
-
+VerificarEstado();
 
 //CRUD con Cloud Firestore
-
 var db = firebase.firestore();
-var coleccion_usuarios = "usuarios";
 var coleccion_estudiantes = "estudiantes";
 var documento;
 
-function CrearUsuario(uid_, email_, nombre_, foto_) {
-  if (nombre_ == undefined) {
-    let data = { email: email_ }
-    db.collection(coleccion_estudiantes).doc(String(uid_)).set(data);
-  }
-  else {
-    db.collection(coleccion_usuarios).add({
-      UID: String(uid_),
-      email: String(email_),
-      nombre: String(nombre_),
-      fotoURL: String(foto_)
-    })
-      .then(function (docRef) {
-        documento = docRef.id;
-        console.log("Usuario creado con ID: ", docRef.id);
-        LeerDatosUsuario(documento);
-      })
-      .catch(function (error) {
-        console.error("Error agregando usuario: ", error);
-      });
-  }
-}
-
-function LeerDatosUsuario(documento_) {
-  let docRef = db.collection(coleccion_estudiantes).doc(documento_);
-  docRef.get().then(function (doc) {
-    if (doc.exists) {
-      console.log("Datos del Documento:", doc.data());
-    } else {
-      console.log("No existe el documento");
+//Función para agregar usuarios a la colección estudiantes 
+function CrearUsuario(email_, nombre_, foto_) {
+    let data = { 
+      correo: String(email_) ,
+      foto: String(foto_) ,
+      nombre: String(nombre_)
     }
-  }).catch(function (error) {
-    console.log("Error obteniendo el documento:", error);
-  });
+    db.collection(coleccion_estudiantes).doc(email_).set(data);
+    return true;
 }
 
-function ActualizarDatosUsuario(coleccion_, documento_, nombre_, foto_) {
-  let datos = db.collection(coleccion_).doc(documento_);
+function ActualizarDatosUsuario(coleccion_, email_, nombre_, foto_) {
+  let datos = db.collection(coleccion_).doc(email_);
   return datos.update({
-    nombre: nombre_,
-    fotoURL: foto_,
+    correo: String(email_) ,
+    foto: String(foto_) ,
+    nombre: String(nombre_)
   })
     .then(function () {
       console.log("BD actualizada");
+      return true;
     })
     .catch(function (error) {
       console.error("Error actualizando documento: ", error);
+      return false;
     });
-}
-
-function EliminarUsuario(coleccion_, documento_) {
-  db.collection(coleccion_).doc(documento_).delete().then(function () {
-    console.log("Usuario elminado");
-  }).catch(function (error) {
-    console.error("Error eliminando usuario: ", error);
-  });
 }
 
 // Varibles de acceso
 
 var user = firebase.auth().currentUser;
 var provider = new firebase.auth.GoogleAuthProvider();
-var name, email, photoUrl, emailVerified, uid;
+var name, email, photoUrl;
 
 function ComprobarDominio(stringemail) {
   if (stringemail.endsWith("@cun.edu.co")) {
@@ -103,30 +56,29 @@ function AccederConCuentaGoogle() {
   //Sugiere el ingreso o registro unicamente con correos con dominio cun.edu.co
   provider.setCustomParameters({ hd: 'cun.edu.co' });
 
+  //Ingreso con ventana emergente de Google
   firebase.auth().signInWithPopup(provider).then(function (result) {
-    var token = result.credential.accessToken;
     email = result.user.email;
     name = result.user.displayName;
     photoUrl = result.user.photoURL;
-    uid = result.user.uid;
 
     if (ComprobarDominio(email) == true) {
 
-      let EstudiantesRef = db.collection(coleccion_estudiantes).doc(result.user.uid);
+      let EstudiantesRef = db.collection(coleccion_estudiantes).doc(email);
       EstudiantesRef.get().then(function (doc) {
         if (doc.exists) {
           //Actualizar datos de usuario que ya existe en la coleccion estudiantes
-          ActualizarDatosUsuario(coleccion_estudiantes, result.user.uid, name, photoUrl);
-          document.getElementById('BotonSalir').style.visibility = 'visible';
+          if(ActualizarDatosUsuario(coleccion_estudiantes, email, name, photoUrl)){
+            VerificarEstado();
+          }
           console.log("El documento ya existia y se actualizo");
 
         } else {
-          //Agregar usuario a la coleccion usuarios
-          CrearUsuario(result.user.uid, email, name, result.user.photoURL);
           //Agregar usuario a la coleccion estudiantes
-          CrearUsuario(result.user.uid, email);
-          document.getElementById('BotonSalir').style.visibility = 'visible';
-          console.log("Correo válido, usuario creado en las 2 colecciones");
+          if(CrearUsuario(email, name, result.user.photoURL)){
+            VerificarEstado();
+          }
+          console.log("Correo válido, usuario creado en la colección estudiantes");
         }
       });
     }
@@ -135,20 +87,27 @@ function AccederConCuentaGoogle() {
       alert("El correo ingresado no es de dominio @cun.edu.co. Por favor, ingrese un correo válido. Ejemplo: juan_perez@cun.edu.co");
       var usuarioaEliminar = firebase.auth().currentUser;
       usuarioaEliminar.delete().then(function () {
-        //Se elimina el usuario de Auth
+        //Se elimina el usuario de Auth cuando el correo ingresado no es de dominio CUN
       }).catch(function (error) {
+        var errorMessg = error.message;
+        console.log("Error al eliminar", errorMessg);
       });
     }
 
   }).catch(function (error) {
     // Datos de error.
-    var errorCode = error.code;
     var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
     console.log("Error de autenticación", errorMessage);
   });
 }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-
+function VerificarEstado() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      setTimeout(function () { location.href = "inicio.html"; }, 0)
+      console.log("usuario activo")
+    } else {
+      console.log("usuario inactivo")
+    }
+  });
+}
