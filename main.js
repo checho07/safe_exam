80b748f395fee71bb4b7aada18e796394b2cea96
Javier Garcia <javier.garcia9611@gmail.com>
2020-09-22 18:22:08 -0500

    Se implementa window.addEventListener con la parámetros offline y online para detectar cuando se pierde la conexión a internet y cuando se recupera, ambos eventos al dispararse se ejecutan las funciones mostrarToast y cerrarToast donde se cambia el estilo del contenedor toast de inicio.html ejecutando las animaciones de cada evento. Se agrega un condicional que verifica si existe en localStorage la llave offline, esta llave es generada en main-examen.js con la hora en la que se pierde la conexión a internet, asi, si la llave no es nula, se actualiza Cloud Firestore con la hora en la que el usuario perdió la conexión a internet. Por último, se agrega la función organizarCards en la que se organiza en ArregloCards por fecha de cierre y el color de la tarjeta en donde primero se muestran los exámenes en curso, luego los activos y finalmente los que ya se han presentado o han cerrado

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var db = firebase.firestore();
 var doc_id;
 var datos = "";
+var ArregloCards = [[],[],[]];
 
 //Dirigirse a examen.html y crear en localstorage una llave ref con valor del id del quiz
 function refExamen(id) {
@@ -32,26 +33,42 @@ function estadoNota(est, not) {
 }
 
 //Función escritura de CRUD para actualizar datos de un documento en la colección quiz  
-function ActualizarDatosUsuario(email_, id_, estado_, nota_) {
+function ActualizarDatosUsuario(email_, id_, estado_, nota_, fechaFinal_) {
     let datos = db.collection("estudiantes").doc(email_).collection("quiz").doc(id_);
-    return datos.update({
-        activo: estado_,
-        nota: nota_
-    })
-        .then(function () {
-            console.log("BD actualizada");
-            return true;
+    if (fechaFinal_ !== undefined) {
+        datos.update({
+            activo: estado_,
+            nota: nota_,
+            horaFin: fechaFinal_
         })
-        .catch(function (error) {
-            console.error("Error actualizando documento: ", error);
-            return false;
-        });
+            .then(function () {
+                console.log("BD actualizada");
+                return true;
+            })
+            .catch(function (error) {
+                console.error("Error actualizando documento: ", error);
+                return false;
+            });
+    } else {
+        datos.update({
+            activo: estado_,
+            nota: nota_,
+        })
+            .then(function () {
+                console.log("BD actualizada");
+                return true;
+            })
+            .catch(function (error) {
+                console.error("Error actualizando documento: ", error);
+                return false;
+            });
+    }
 }
 
 //Función para cerrar sesión
 function SalirDeLaCuenta() {
     firebase.auth().signOut().then(function () {
-        setTimeout(function () { location.href = "index.html"; }, 1000);
+        setTimeout(function () { location.href = "index.html"; }, 0);
         // Sign-out successful.
     }).catch(function (error) {
         // An error happened.
@@ -62,14 +79,14 @@ function SalirDeLaCuenta() {
 function CrearTarjeta(datos) {
     let contenedor = `<div class="row">` + datos + `</div>`;
     document.getElementById("contenedorQuices").innerHTML = contenedor;
-    return contenedor;
 }
 
-function Card(asign, fechaf, nomb, nota, estado, color, dir) {
-    datos += `
-    <div class="col-md-3 col-sm-6 item d-flex align-items-stretch">
-      <div class="card" `+dir+`">
-        <img class="card-img-top" src="https://fakeimg.pl/350x200/?text=_" alt="Card image cap">
+function Card(asign, fechaf, nomb, nota, estado, color, dir, epoch) {
+    
+    let codigoCard = `
+    <div class="col-md-4 col-sm-6 col-lg-3 item d-flex align-items-stretch">
+      <div class="card" `+ dir + `">
+        <img class="card-img-top" src="https://picsum.photos/400/300?random" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">`+ asign + `</h5>
             <p class="card-text">`+ nomb + `</p>
@@ -81,83 +98,169 @@ function Card(asign, fechaf, nomb, nota, estado, color, dir) {
         </div>
       </div>
     </div>`;
-    CrearTarjeta(datos);
-}
 
+    organizarCards(codigoCard,epoch,color);  
+
+    CrearTarjeta(datos);
+    
+}
 //Lectura y escritura de los quizes del usuario actual, actualización de estados y notas 
 function GenerarModulos() {
     firebase.auth().onAuthStateChanged(function (user) {
-        if(user){
-        var horaAct = new Date(Date.now());
-        horaAct = (horaAct[Symbol.toPrimitive]('number')) / 1000; //Hora actual en milisegundos
-        doc_id = user.email;
-        db.collection("estudiantes").doc(doc_id).collection("quiz").get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                var data = doc.data();
-                var ref = data.id;         //ID del documento del quiz
-                var nota = data.nota;      //Nota quiz
-                var estado = data.activo;  //Estados del quiz
+        if (user) {
+            var horaAct = new Date(Date.now());
+            horaAct = (horaAct[Symbol.toPrimitive]('number')) / 1000; //Hora actual en milisegundos
+            doc_id = user.email;
+            db.collection("estudiantes").doc(doc_id).collection("quiz").onSnapshot(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    ArregloCards = [[],[],[]];
+                    var data = doc.data();
+                    var ref = data.id;         //ID del documento del quiz
+                    var nota = data.nota;      //Nota quiz
+                    var estado = data.activo;  //Estados del quiz
 
-                db.collection("quiz").doc(ref).get().then(function (dataQuiz) {
-                    var dataQ = dataQuiz.data();                           
-                    var horaFinalQuiz = localStorage.getItem(ref);       //Hora final del quiz que esta en curso
-                    var tiempoRest = tiempoRelativo(horaFinalQuiz/1000); //Calculo de tiempo del quiz en curso con moment.js
-                    var asign = dataQ.asignatura;                        //Nombre de la asignatura
-                    var fechaf = tiempoRelativo(dataQ.fechafinal);       //Calculo de tiempo para cierre de quiz con moment.js
-                    var nomb = dataQ.nombre;                             //Nombre del quiz                                     
-                    var col;                                             //Color del footer de cada card
-                    var conDir = `onclick="refExamen(`+`'`+ref+`'`+`)"`; //Cadena de evento de un quiz que esta activo o en curso
-                    var sinDir = `style="cursor:default;"`;              //Cadena para quiz inactivo
+                    db.collection("quiz").doc(ref).get().then(function (dataQuiz) {
+                        var dataQ = dataQuiz.data();
+                        var horaFinalQuiz = localStorage.getItem(ref);       //Hora final del quiz que esta en curso
+                        var tiempoRest = tiempoRelativo(horaFinalQuiz / 1000); //Calculo de tiempo del quiz en curso con moment.js
+                        var asign = dataQ.asignatura;                        //Nombre de la asignatura
+                        var fechaf = tiempoRelativo(dataQ.fechafinal);       //Calculo de tiempo para cierre de quiz con moment.js
+                        var nomb = dataQ.nombre;                             //Nombre del quiz                                     
+                        var col;                                             //Color del footer de cada card
+                        var conDir = `onclick="refExamen(` + `'` + ref + `'` + `)"`; //Cadena de evento de un quiz que esta activo o en curso
+                        var sinDir = `style="cursor:default;"`;              //Cadena para quiz inactivo
 
-                    // Caso de quiz activo pero no en curso
-                    if (data.activo === true && localStorage.getItem(ref) == null) {
+                        // Caso de quiz activo pero no en curso
+                        if (data.activo === true && localStorage.getItem(ref) == null) {
 
-                        // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual
-                        if (dataQ.fechafinal < horaAct) {
-                            ActualizarDatosUsuario(doc_id,data.id,false,"0"); //Se actualiza bd, se cierra el quiz con nota 0
-                            col = "bg-danger";
-                            Card(asign, fechaf, nomb, "0", false, col,sinDir); }
-                          
-                        // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual    
-                        else {
-                            col = "bg-success";
-                            Card(asign, fechaf, nomb, nota, estado, col,conDir); }
+                            // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual
+                            if (dataQ.fechafinal < horaAct) {
+                                ActualizarDatosUsuario(doc_id, data.id, false, "0"); //Se actualiza bd, se cierra el quiz con nota 0
+                                col = "bg-danger";
+                                Card(asign, fechaf, nomb, "0", false, col, sinDir,dataQ.fechafinal);
+                            }
 
-                    // Caso de quiz inactivo y ya presentado
-                    } else if (data.activo === false && localStorage.getItem(ref) == null) {
+                            // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual    
+                            else {
+                                col = "bg-success";
+                                Card(asign, fechaf, nomb, nota, estado, col, conDir,dataQ.fechafinal);
+                            }
 
-                        // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual
-                        if (dataQ.fechafinal < horaAct) {
-                        col = "bg-danger";
-                        Card(asign, fechaf, nomb, nota, estado, col, sinDir); }
-                        
-                        // Caso en que el quiz ya se haya presentado
-                        else{
-                        col = "bg-danger";
-                        Card(asign,"Ya fue presentado", nomb, nota, estado, col, sinDir); }
-
-                    // Caso de quiz inactivo pero aún en curso
-                    } else if (data.activo === false && localStorage.getItem(ref) !== null) {
-                        console.log(localStorage.getItem(data.id))
-                        // Caso en que la hora en que debia terminar el quiz es menor a la hora actual
-                        if (horaFinalQuiz < (horaAct*1000)) {
+                            // Caso de quiz activo pero se cambio de navegador
+                        } else if (data.activo === true && localStorage.getItem(ref) !== null) {
                             localStorage.removeItem(ref);
-                            ActualizarDatosUsuario(doc_id,data.id,false,"0");
-                            col = "bg-danger";
-                            Card(asign, tiempoRest, nomb, "0", false, col, sinDir); }
+                            // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual
+                            if (dataQ.fechafinal < horaAct) {
+                                ActualizarDatosUsuario(doc_id, data.id, false, "0"); //Se actualiza bd, se cierra el quiz con nota 0
+                                col = "bg-danger";
+                                Card(asign, fechaf, nomb, "0", false, col, sinDir,dataQ.fechafinal);
+                            }
 
-                        // Caso en que el quiz sigue en curso y el timempo no ha terminado
-                        else {
-                            col = "bg-warning";
-                            Card(asign, tiempoRest, nomb, nota, "En curso", col, conDir); }
-                    }
+                            // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual    
+                            else {
+                                col = "bg-success";
+                                Card(asign, fechaf, nomb, nota, estado, col, conDir,dataQ.fechafinal);
+                            }
+
+                            // Caso de quiz inactivo y ya presentado
+                        } else if (data.activo === false && localStorage.getItem(ref) == null) {
+
+                            // Caso en que la hora en que debia cerrarse el quiz es menor a la hora actual
+                            if (dataQ.fechafinal < horaAct) {
+                                col = "bg-danger";
+                                Card(asign, fechaf, nomb, nota, estado, col, sinDir,dataQ.fechafinal);
+                            }
+
+                            // Caso en que el quiz ya se haya presentado
+                            else {
+                                col = "bg-danger";
+                                Card(asign, "Ya fue presentado", nomb, nota, estado, col, sinDir,dataQ.fechafinal);
+                            }
+
+                            // Caso de quiz inactivo pero aún en curso
+                        } else if (data.activo === false && localStorage.getItem(ref) !== null) {
+                            //console.log(localStorage.getItem(data.id))
+                            // Caso en que la hora en que debia terminar el quiz es menor a la hora actual
+                            if (horaFinalQuiz < (horaAct * 1000)) {
+                                if(localStorage.getItem("offline") !== null){
+                                    let datos = db.collection("estudiantes").doc(doc_id).collection("quiz").doc(data.id);
+                                    let offline = localStorage.getItem("offline");
+                                    datos.update({
+                                        horaOffline: firebase.firestore.Timestamp.fromMillis(offline),
+                                        horaOnline: firebase.firestore.Timestamp.now()
+                                    }).then(function () {localStorage.removeItem("offline")})
+                                } 
+                                localStorage.removeItem(ref);
+                                ActualizarDatosUsuario(doc_id, data.id, false, "0", firebase.firestore.Timestamp.now());
+                                col = "bg-danger";
+                                Card(asign, tiempoRest, nomb, "0", false, col, sinDir,dataQ.fechafinal);
+                            }
+
+                            // Caso en que el quiz sigue en curso y el timempo no ha terminado
+                            else {
+                                if(localStorage.getItem("offline") !== null){
+                                    let datos = db.collection("estudiantes").doc(doc_id).collection("quiz").doc(data.id);
+                                    let offline = localStorage.getItem("offline");
+                                    datos.update({
+                                        horaOffline: firebase.firestore.Timestamp.fromMillis(offline),
+                                        horaOnline: firebase.firestore.Timestamp.now()
+                                    }).then(function () {localStorage.removeItem("offline")})
+                                } 
+                                col = "bg-warning";
+                                Card(asign, tiempoRest, nomb, "0", "En curso", col, conDir,dataQ.fechafinal);
+                            }
+                        }
+                    });
                 });
             });
-        });
-    }else{
-        SalirDeLaCuenta();
-    }
+        } else {
+            SalirDeLaCuenta();
+        }
     })
 }
 GenerarModulos();
 
+// Evento de conexión perdida
+window.addEventListener("offline", function (e) {
+    console.log("Conexión perdida");
+    mostrarToast();
+});
+
+// Evento de conexión En línea
+window.addEventListener("online", function (e) {
+    console.log("En línea");
+    cerrarToast();
+});
+
+function mostrarToast() {
+    var toast = document.getElementById("mitoast");
+    toast.className = "mostrar";
+}
+
+function cerrarToast() {
+    var toast = document.getElementById("mitoast");
+    toast.className = "cerrar";
+    toast.className = toast.className.replace("cerrar", "");
+}
+
+function organizarCards(stringCard, fecha, color){
+    datos="";
+    if(color == "bg-warning"){
+        ArregloCards[0].push([fecha,stringCard]);
+        ArregloCards[0].sort();
+    }
+    else if(color == "bg-success"){
+        ArregloCards[1].push([fecha,stringCard]);
+        ArregloCards[1].sort();
+    }
+    else if(color == "bg-danger"){
+        ArregloCards[2].push([fecha,stringCard]);
+        ArregloCards[2].sort();
+    }
+    
+    for(let i = 0; i < ArregloCards.length; i++){
+        for(let j = 0; j < ArregloCards[i].length; j++){
+            datos += ArregloCards[i][j][1];
+        }
+    }
+}
