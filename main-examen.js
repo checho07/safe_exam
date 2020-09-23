bd63a9e9731ea0cb62ae24f4e8125f31186fe198
Javier Garcia <javier.garcia9611@gmail.com>
2020-09-22 19:51:35 -0500

    Se agrega la función bloqueoNavegador() que advierte por medio de la alerta del navegador cuando el usuario quiere retroceder o recargar la página o cerrar el navegador. Al igual que en main.js se implementa window.addEventListener con la parámetros offline y online para detectar cuando se pierde la conexión a internet y cuando se recupera, con la modificación de que en caso offline se crea una llave en localStorage de nombre offline con la hora en UTC-5 en que se pierde la conexión. En el caso online se agrega esa hora en Cloud Firestore el documento del quiz con el nombre horaOffline la hora alamcenada en localStorage conviertiendola en timeStamp y también se agrega la hora de reconexión al campo horaOnline con la hora actual obtenida con timeStamp. Se crea la función obtenerIP() que utiliza la api ipify que retorna en un json la dirección IP local del usuario y es almacenada en la variable local dirIP la cual es almacenada en Cloud Firestore una vez se ejecuta la función Continuar(). Se crea la función respuestaRandom() la cual genera un arreglo con las respuestas de cada pregunta del quiz para posteriormente reorganizarlo aleatoriamente con math.random y al tiempo ingresando en el arreglo respuestas la respuesta correcta al nuevo orden de las respuestas. Luego este arreglo de orden aleatorio es retornado para ser utilizado en la función CrearExamen(). Con el mismo método en la línea de código 116 son organizadas las preguntas del quiz de forma aleatoria, generando un nuevo arreglo el cual puede ser modificado para definir cuantas preguntas se quieren mostrar según lo que haya escogido el docente.

--- main-examen.js
+++ main-examen.js
@@ -1,18 +1,49 @@
 var db = firebase.firestore();
-var coleccion_curso = "curso";
-var coleccion_examenes_asoc = "examenes-asociados";
-var email, campos, curso, examen, docRef, tiempoBd;
+var email, tiempoBd, dirIP;
 var focus = true;
 var referencia = "";
 //Arreglo que asigna la letra correspondiente al orden de cada pregunta generada
-var indicadores = ['a','b','c','d','e','f','g','h','i','j','k','l'];
+var indicadores = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 //Variable en la que se almacenan las respuestas asignadas por el docente
 var respuestas = [];
 
 if (localStorage.getItem("ref") === null) {Volver();}
 else{referencia=localStorage.getItem("ref");} 
 
+function respuestaRandom(arreglo, posRespuesta) {
+    let Respuesta;
+    for(let i = 0; i < indicadores.length; i++){
+        if(indicadores[i] == posRespuesta){
+            Respuesta = arreglo[i+1];
+        }
+    }
+
+    let nuevoArreglo = [];
+    for(let i = 1; i < arreglo.length - 1; i++){
+        nuevoArreglo[i-1] = arreglo[i];
+    }
+
+    let temporal, posRandom;
+    for (let i = nuevoArreglo.length; i > 1; i--) {
+        posRandom = Math.floor(Math.random() * i);
+        temporal = nuevoArreglo[posRandom];
+        nuevoArreglo[posRandom] = nuevoArreglo[i-1];
+        nuevoArreglo[i-1] = temporal;
+    }
+
+    for (let i = 0; i < nuevoArreglo.length; i++){
+        if(nuevoArreglo[i] == Respuesta){
+            respuestas.push(indicadores[i]);
+            //console.log("respuestas correctas",respuestas);
+        }
+    }
+    return nuevoArreglo;
+}
+
 function ActualizarDatosUsuario(email_, id_, estado_, nota_) {
+    clearInterval(TiempoRestante);
+    document.body.onbeforeunload = "";
+    localStorage.removeItem(referencia);
     let datos = db.collection("estudiantes").doc(email_).collection("quiz").doc(id_);
     return datos.update({
         activo: estado_,
@@ -20,11 +51,15 @@ function ActualizarDatosUsuario(email_, id_, estado_, nota_) {
         horaFin: firebase.firestore.Timestamp.now()
     })
         .then(function () {
+            if(document.body.className === "focused"){
             //Se elimina el almacenanmiento local y se redirige
-            clearInterval(TiempoRestante);
-            localStorage.removeItem(referencia);
             setTimeout(function () { location.href = "inicio.html"; }, 5000);
-            console.log("BD actualizada");
+            }else{
+                window.onfocus = function () {
+                    setTimeout(function () { location.href = "inicio.html"; }, 5000);
+                    console.log("BD actualizada");
+                }
+            }
         })
         .catch(function (error) {
             console.error("Error actualizando documento: ", error);
@@ -38,15 +73,15 @@ function CrearExamen(nombre, arreglo, id, rpta) {
     //La posición 0 es el nombre de la pregunta 
     //La posición 1 en adeltante hasta la penúltima posoción, son las preguntas
     //La última posición es la respuesta correcta
+    let arregloRespuestas = respuestaRandom(arreglo, rpta);
     let preg = "";
-    respuestas.push(rpta);
     for (let i = 1; i < arreglo.length - 1; i++) {
-        preg += `<li><label><input type="radio" name="`+id+`" value=` + indicadores[i - 1] + `>` + arreglo[i] + `</label></li>`;
+        preg += `<li><label><input type="radio" name="`+id+`" value=` + indicadores[i - 1] + `>` + arregloRespuestas[i-1] + `</label></li>`;
     }
 
     let contenedor = `
     <div class="card quizlib-question">
-    <div class="quizlib-question-title">`+ nombre + `</div>
+    <div class="quizlib-question-title"><h4>`+ nombre + `</h4></div>
     <div class="quizlib-question-answers">
         <ul>`+ preg + `
         </ul>
@@ -55,7 +90,6 @@ function CrearExamen(nombre, arreglo, id, rpta) {
         `;
 
     document.getElementById('quiz').style.visibility = 'hidden';
-    console.log(respuestas);
     return contenedor;
 
 }
@@ -64,22 +98,41 @@ firebase.auth().onAuthStateChanged(function (user) {
     //Si hay usuario activo
     if (user) {
         email = user.email;
+        obtenerIP();
         db.collection("quiz").doc(referencia).get().then(function (doc) {
             if (doc.exists) {
                 var dataDoc = doc.data();
                 var preg = dataDoc.preguntas;
                 tiempoBd = dataDoc.tiempo;
                 document.getElementById("titulo").innerHTML = "<h1>"+dataDoc.nombre+"</h1>";
-                //Ciclo que se repite por cada llave dentro del objeto data que son las preguntas del exámen
+                var arregloAux = [];
+                
+                //Se crea un nuevo arreglo con las datos del objeto preguntas del quiz
                 for (const prop in preg) {
-                    var arreglo = preg[prop];
-                    console.log("arr: ", arreglo);
-                    console.log(`obj.${prop} = ${preg[prop]}`);
+                    arregloAux.push(preg[prop]);
+                } 
+
+                //Ciclo que cambia el orden de forma aleatoria del arreglo anteriormente creado
+                let temporal, posRandom;
+                for (let i = arregloAux.length; i > 1; i--) {
+                    posRandom = Math.floor(Math.random() * i);
+                    temporal = arregloAux[posRandom];
+                    arregloAux[posRandom] = arregloAux[i-1];
+                    arregloAux[i-1] = temporal;
+                }
+                //console.log("auxiliar",arregloAux);
+
+                //Ciclo que se repite por cada llave dentro del arreglo auxiliar en donde prop es el arreglo con el encabezados,respuestas y rpta correcta
+                for (const prop in arregloAux) {
+                    var arreglo = arregloAux[prop];
+                    //console.log("arr: ", arreglo);
+                    //console.log(`obj.${prop} = ${arregloAux[prop]}`);
                     //Se insertan los bloques HTML del quiz junto con las preguntas importadas de firebase
                     document.getElementById("quiz").innerHTML += CrearExamen(arreglo[0], arreglo, prop, arreglo[arreglo.length - 1]);
                 } 
                 document.getElementById("cont").disabled = false;
                 document.getElementById("cont").style.background = "#44b847";
+                document.getElementById("cover-loader").style.display = "none";
                 //Una vez insertado todo el código HTML se habilita el botón continuar
             }
         })
@@ -109,7 +162,7 @@ function CuentaRegresiva() {
         //Se asigna el tiempo de duración del exámen y se calcula a que hora según UTC debe finalizar
         tiempo_examen = tiempoBd;
         var t_miliseg = tiempo_examen * 1000;
-        var fecha = Date.now()
+        var fecha = Date.now();
         var horaActual = new Date(fecha);
         var horaInicio = horaActual.getTime();
         var horaFin = horaInicio + t_miliseg;
@@ -121,10 +174,10 @@ function CuentaRegresiva() {
         var horaReingreso = new Date(Date.now());  
         var tiempo_examen = (localStorage.getItem(referencia) - horaReingreso.getTime()); 
     }
-    console.log("este es: ",localStorage.getItem(referencia));
+    //console.log("este es: ",localStorage.getItem(referencia));
     //Se pasa el tiempo a segundos
     var tActual = tiempo_examen/1000;
-    console.log(tActual);
+    //console.log(tActual);
     //Se convierte el tiempo actual en minutos y horas
     var minutos = Math.round((tActual - 30) / 60) % 60;
     var segundos = Math.round(tActual) % 60;
@@ -144,11 +197,15 @@ function CuentaRegresiva() {
     }
     else {
 
+        //Método que hablita la advertencia de los botones de regreso, recargar y cerrar del navegador
+        if (document.body.onbeforeunload !== "") {
+            bloqueoNavegador();
+        }
+
         //Método que detecta si el usuario abre otra pestaña o cambia de programa
         window.onblur = function () {
             document.body.className = 'blurred';
             TiempoTerminado();
-            //alert("El examen ha sido cancelado, ha abierto una pestaña o ventana diferente a la del examen");
         };
 
         //Disminucón unitaria del tiempo cada 1000 milseg, actualización del valor de hora actual en localStorage
@@ -161,15 +218,19 @@ var TiempoRestante;
 
 //Función de botón continuar de la alerta incial "recuerde"
 function Continuar() {
+    document.body.setAttribute("onbeforeunload", `"bloqueoNavegador()"`);
     let datos = db.collection("estudiantes").doc(email).collection("quiz").doc(referencia);
+    document.getElementById("cover-loader").style.display = "block";
     if (localStorage.getItem(referencia) == null) {
         datos.update({
-            horaInicio: firebase.firestore.Timestamp.now()
+            horaInicio: firebase.firestore.Timestamp.now(),
+            IP: dirIP
         })
     }
 
     datos.update({ activo: false, }).then(function () {
         //Se incia la cuenta regresiva actualizando la función cada 1000 milseg, se muestra el contenido del exámen
+        document.getElementById("cover-loader").style.display = "none";
         TiempoRestante = setInterval('CuentaRegresiva()', 1000);
         EliminarContenedor("recuerde");
         document.getElementById('titulo').style.visibility = 'visible';
@@ -189,8 +250,10 @@ function TiempoTerminado() {
 //Revisión del exámen con funciones de la libreria quizlib
 function Revision() {
     var quiz = new Quiz('quiz', respuestas);
+    setTimeout(`document.getElementById("cover-loader").style.display = "block"`,4000);
     // checkAnswers retorna true si todas las pregunsta fueron respondida
     if (quiz.checkAnswers(true)) {
+        window.scrollTo(0, document.body.scrollHeight); //Scroll al fondo de la página para observar mensaje
         let quizScorePercent = (quiz.result.scorePercentFormatted*5)/100; //Nota
         let quizResultElement = document.getElementById('quiz-result');
         quizResultElement.style.display = 'block';
@@ -204,14 +267,17 @@ function Revision() {
             document.getElementById('quiz-mssg').innerHTML = "Intento finalizado. La nota será publicada luego de la revisión.";
         } else if(tiempo_examen == 0 && document.body.className == "focused"){
             quizResultElement.style.backgroundColor = '#f44336';
+            window.scrollTo(0, document.body.scrollHeight); //Scroll al fondo de la página para observar mensaje
             document.getElementById('quiz-mssg').innerHTML = "El tiempo ha terminado. La nota será publicada luego de la revisión.";
         } else if(tiempo_examen == 0 && document.body.className == 'blurred'){
             quizResultElement.style.backgroundColor = '#f44336';
+            window.scrollTo(0, document.body.scrollHeight); //Scroll al fondo de la página para observar mensaje
             document.getElementById('quiz-mssg').innerHTML = "El examen ha sido cancelado, ha abierto una pestaña o ventana diferente a la del examen. ";
         }
 
     //checkAnswers es false cuando faltan preguntas por responder
     }else if(quiz.checkAnswers(false)){
+        window.scrollTo(0, document.body.scrollHeight); //Scroll al fondo de la página para observar mensaje
         let quizScorePercent = (quiz.result.scorePercentFormatted*5)/100; //Nota
         let quizResultElement = document.getElementById('quiz-result');
         quizResultElement.style.display = 'block';
@@ -245,12 +311,65 @@ function Volver() {
 //Evento que bloquea el click derecho
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-//Método que verifica si el usuario puede usar localStorage
-/*if (typeof(Storage) !== "undefined") {
-            // Store
-            localStorage.setItem("tiempoActual",tActual);
-            // Retrieve
-            console.log(localStorage.getItem("tiempoActual"));
-          } else {
-            alert("Sorry, your browser does not support Web Storage...");
-          }*/
+// Evento de conexión perdida
+window.addEventListener("offline", function (e) {
+    console.log("Conexión perdida");
+    localStorage.setItem("offline",Date.now());
+    let toast = document.getElementById("toast");
+    toast.style.background = "#dc3545";
+    toast.innerHTML = "Se ha perdido la conexión a internet";
+    toast.style.visibility = "visible";
+    toast.className = "mostrar";
+});
+
+// Evento de conexión En línea
+window.addEventListener("online", function (e) {
+    console.log("En línea");
+    let offline = localStorage.getItem("offline");
+    let datos = db.collection("estudiantes").doc(email).collection("quiz").doc(referencia);
+    if (localStorage.getItem("offline") !== null) {
+        datos.update({
+            horaOffline : firebase.firestore.Timestamp.fromMillis(offline),
+            horaOnline  : firebase.firestore.Timestamp.now()
+        })
+    }
+    let toast = document.getElementById("toast");
+    toast.style.background = "#44b847";
+    toast.className = "ocultar";
+    toast.innerHTML = "Conexión recuperada";
+    setTimeout(function(){ toast.className = toast.className.replace("ocultar", ""); toast.style.visibility = "hidden"; }, 3000);
+});
+
+//Función para obtener la IP pública del usuario
+function obtenerIP() {
+    $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function (data) {
+        dirIP = data["ip"];
+    });
+}
+
+//Función para advertir cancelación del exámen en botones de regreso o cerrar del navegador
+function bloqueoNavegador() {  
+    window.onbeforeunload = function (evento) {
+        mensaje = "¿Desea abandonar el sitio?";
+        evento.returnValue = mensaje;
+        return mensaje;
+      };
+}
+
+//Prueba de función para advertir con javascript
+/*function bloqueoNavegador() {  
+    window.addEventListener("beforeunload", function(event) {
+        event = setInterval(
+            function () {
+                var mensaje = document.getElementById("mensajeAlerta");
+                mensaje.style.visibility = "visible";
+                mensaje.innerHTML = 
+                `<div class="mensajeAlerta-header">Aviso</div>
+                <span class="closebtn" onclick="this.parentElement.style.visibility='hidden';">×</span>
+                <div class="mensajeAlerta-container">
+                    <p>Esta saliendo de la pagína actual, el examen será calificado con las respuestas contestadas ¿Está seguro que desea salir?</p>
+                </div>`; 
+            }, 10000
+          );
+    });
+}*/
